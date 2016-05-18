5993183b38e58471a9f1cf313dedf7d224ad51a4
antonm <antonm@ca.ibm.com>
2016-05-18 17:30:10 -0400

    style tweaks, db tweaks, travis, data model

--- app.js
+++ app.js
@@ -14,19 +14,20 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+var log4js = require('log4js');
+
+var logger = log4js.getLogger('application');
+
 // create a new express server
 var app = express();
-var http=require('http');
+var http = require('http');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
 // serve the files out of ./public as our main files
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
@@ -36,10 +37,6 @@ app.listen(appEnv.port, '0.0.0.0', function () {
 
     // print a message when the server starts listening
     console.log("server starting on " + appEnv.url);
-
-
-
-
 
     //Home page
     app.get("/", function (req, res) {
@@ -159,11 +156,6 @@ app.listen(appEnv.port, '0.0.0.0', function () {
     });
 
 
-
-
-
-
-
     //----------------------------------------------------------------------------------
     // Cloudant connections
     //----------------------------------------------------------------------------------    
@@ -184,23 +176,31 @@ app.listen(appEnv.port, '0.0.0.0', function () {
         console.log('VCAP_SERVICES: %s', process.env.VCAP_SERVICES);
         // Also parse Cloudant settings.
         var credentials = env['cloudantNoSQLDB'][0]['credentials'];
-        username =credentials.username;
+        username = credentials.username;
         password = credentials.password;
 
-    // Initialize the library with CloudCo account.
-     cloudant = Cloudant({
-        account: username,
-        password: password
-    });
+        // Initialize the library with CloudCo account.
+        cloudant = Cloudant({
+            account: username,
+            password: password
+        });
     }
-  /*  var username = process.env.cloudant_username;
-    var password = process.env.cloudant_password;
+    /*  var username = process.env.cloudant_username;
+      var password = process.env.cloudant_password;
 
-    // Initialize the library with CloudCo account.
-    var cloudant = Cloudant({
-        account: username,
-        password: password
-    });*/
+      // Initialize the library with CloudCo account.
+      var cloudant = Cloudant({
+          account: username,
+          password: password
+      });*/
+
+    cloudant.db.create("insurance", function (err, res) {
+        if (err) {
+            logger.warn('database already created');
+        } else {
+            logger.info('database created');
+        }
+    });
 
     cloudant.db.list(function (err, allDbs) {
         console.log('All my databases: %s', allDbs)
@@ -208,8 +208,6 @@ app.listen(appEnv.port, '0.0.0.0', function () {
 
     //use Insurance DB
     var db = cloudant.db.use("insurance");
-
-
 
     //Create Index
     app.post('/insurance/createindex', function (req, res) {
@@ -234,7 +232,7 @@ app.listen(appEnv.port, '0.0.0.0', function () {
 
     //Quering with a query string
     // localhost:6001/insurance/query?payername=John+Appleseed -- pass a payername as query string.
-    // localhost:6001/insurance/query -- Will pick default user for now.
+    // localhost: 6001 / insurance / query -- Will pick default user for now.
     app.get("/insurance/query", function (req, res) {
         db.find({
             selector: {
