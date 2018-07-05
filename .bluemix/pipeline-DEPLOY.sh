d70cac0d0650464780daa58a6f9c4518c28340d9
data-henrik <hloeser@de.ibm.com>
2018-07-05 10:36:35 +0200

    deployment changes for App ID Cloud Directory support

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo Login IBM Cloud api=$CF_TARGET_URL org=$CF_ORG space=$CF_SPACE
+bx login -a "$CF_TARGET_URL" --apikey "$IAM_API_KEY" -o "$CF_ORG" -s "$CF_SPACE"
 
 # The branch may use a custom manifest
 MANIFEST=manifest.yml
@@ -10,47 +12,51 @@ fi
 echo "Using manifest file: $MANIFEST"
 echo "Using prefix: $PREFIX"
 
-# Create CF services
-cf create-service conversation free insurance-bot-conversation
-cf create-service cloudantNoSQLDB Lite ${PREFIX}insurance-bot-db
-cf create-service appid "Graduated tier" insurance-bot-appid
-# Set app's env vars
-domain=".mybluemix.net"
-case "${REGION_ID}" in
-  ibm:yp:eu-gb)
-    domain=".eu-gb.mybluemix.net"
-  ;;
-  ibm:yp:au-syd)
-  domain=".au-syd.mybluemix.net"
-  ;;
-  ibm:yp:eu-de)
-  domain=".eu-de.mybluemix.net"
-  ;;
-esac
-if ! cf app $CF_APP; then
-  cf push $CF_APP -n $CF_APP -f $MANIFEST --no-start
+# Create services
+bx service create conversation free insurance-bot-conversation
+bx service create cloudantNoSQLDB Lite ${PREFIX}insurance-bot-db
+bx service create appid "Graduated tier" insurance-bot-appid
+
+# Set up App ID service
+# Note: We only configure Cloud Directory and don't turn Google / Facebook off
+#
+# Create service key from which to obtain managementUrl
+bx service key-create insurance-bot-appid for-pipeline
+# managementUrl includes tenantId
+APPID_MGMT_URL=`bx service key-show insurance-bot-appid for-pipeline | grep "\"managementUrl\"" | awk '{print $2}' | tr -d '","'`
+# We need the IAM token
+IAM_OAUTH_TOKEN=`bx iam oauth-tokens | sed -n 1p | awk 'NF>1{print $NF}'`
+# Now configure App ID for Cloud Directory
+curl -X PUT --header 'Content-Type: application/json' --header 'Accept: application/json' \
+           --header 'Authorization: Bearer $IAM_OAUTH_TOKEN' \
+           -d @"appid-config.json"  '$APPID_MGMT_URL'
+
+
+# Deploy the app
+if ! bx app show $CF_APP; then
+  bx app push $CF_APP -n $CF_APP -f ${MANIFEST} --no-start
   if [ ! -z "$CONVERSATION_WORKSPACE" ]; then
-    cf set-env $CF_APP CONVERSATION_WORKSPACE $CONVERSATION_WORKSPACE
+    bx cf set-env $CF_APP CONVERSATION_WORKSPACE $CONVERSATION_WORKSPACE
   fi
-  cf start $CF_APP
+  bx cf start $CF_APP
 else
   OLD_CF_APP=${CF_APP}-OLD-$(date +"%s")
   rollback() {
     set +e
-    if cf app $OLD_CF_APP; then
-      cf logs $CF_APP --recent
-      cf delete $CF_APP -f
-      cf rename $OLD_CF_APP $CF_APP
+    if bx app show $OLD_CF_APP; then
+      bx app logs $CF_APP --recent
+      bx app delete $CF_APP -f
+      bx app rename $OLD_CF_APP $CF_APP
     fi
     exit 1
   }
   set -e
   trap rollback ERR
-  cf rename $CF_APP $OLD_CF_APP
-  cf push $CF_APP -n $CF_APP -f $MANIFEST --no-start
+  bx app rename $CF_APP $OLD_CF_APP
+  bx app push $CF_APP -n $CF_APP -f ${MANIFEST} --no-start
   if [ ! -z "$CONVERSATION_WORKSPACE" ]; then
-    cf set-env $CF_APP CONVERSATION_WORKSPACE $CONVERSATION_WORKSPACE
+    bx cf set-env $CF_APP CONVERSATION_WORKSPACE $CONVERSATION_WORKSPACE
   fi
-  cf start $CF_APP
-  cf delete $OLD_CF_APP -f
+  bx cf start $CF_APP
+  bx app delete $OLD_CF_APP -f
 fi
