1da78ca7c056f31992db14b07c2e135c806be63e
Henrik Loeser <hloeser@de.ibm.com>
2019-12-02 13:58:57 +0100

    fix authenticator

--- config/bot.js
+++ config/bot.js
@@ -11,7 +11,7 @@
  *
  */
 var watson = require('ibm-watson/assistant/v1');
-const { IAMAuthenticator } = require('ibm-watson/auth');
+const { IamAuthenticator } = require('ibm-watson/auth');
 var cfenv = require('cfenv');
 var chrono = require('chrono-node');
 var fs = require('fs');
